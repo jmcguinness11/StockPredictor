cb519a79fed6e61091c64c07e3ca7522d93c9f8f
John McGuinness <johnmcguinness@Johns-MacBook-Pro.local>
2018-04-18 19:26:37 -0400

    file assembles tweet dataset assigned to random classes

--- create_tweet_classes.py
+++ create_tweet_classes.py
@@ -4,35 +4,47 @@
 
 import collections
 import json
+import random
 
 refined_tweets = collections.defaultdict(list)
 
+#returns label for company and time
+def getLabel(ticker, month, day, hour):
+	return random.randint(-1,1)
+
 #parses individual json file
-def parseJSON(data):
-	results = collections.defaultdict(int)
+def parseJSON(data, month, day, hour):
+	results = []
 	for tweet in data.itervalues():
-		if tweet['company'] in results: 
-			results[tweet['company']] += 1
-		else:
-			results[tweet['company']] = 0
-	print results
-		
+		text = tweet['text']
+		label = getLabel(tweet['company'], month, day, hour)
+		results.append([text,label])
+	return results
 
 def loadData(months, days):
 	hours = [10, 11, 12, 13, 14]
 	minutes = [0, 15, 30, 45]
+	output_data = []
 	for month in months:
 		for day in days:
 			for hour in hours:
 				for minute in minutes:
 					filename = 'tweets_{}_{}_{}_{}.dat'.format(month, day, hour, minute)
 					with open(filename, 'r') as f:
-						data = json.load(f)
-						parseJSON(data)
+						try:
+							data = json.load(f)
+						except ValueError as err:
+							print filename
+							exit(1)
+						output_data += parseJSON(data, month, day, hour)
 						f.close()
+	print len(output_data)
+	print output_data[0:10]
+	return output_data
 
 def main():
-	loadData([4], [10,11])
+	days = [9,10,11,12,13,16,17]
+	loadData([4], days)
 
 if __name__=='__main__':
 	main()
